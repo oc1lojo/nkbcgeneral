c811b34b44fc6aef19734e86afb2ff9a5c43c146
John Lövrot <john.lovrot@sll.se>
2021-01-28 09:50:23 +0100

    Anpassa till till situation när inte alla registervariabler ingår i datauttaget som bearbetas

--- R/clean_nkbc_data.R
+++ R/clean_nkbc_data.R
@@ -14,13 +14,23 @@ clean_nkbc_data <- function(x, ...) {
     )
 
   # Rensa operationsformulärdata om inte operationsdatum är satt
-  x[is.na(x$op_kir_dat), tidyselect::vars_select(names(x), tidyselect::starts_with("op_"))] <- NA
+  if ("op_kir_dat" %in% names(x)) {
+    x[is.na(x$op_kir_dat), tidyselect::vars_select(names(x), tidyselect::starts_with("op_"))] <- NA
+  }
 
   # Rensa formulärdata om inte pat_sida är vald för formulär
-  x[is.na(x$op_pat_sida_Varde), tidyselect::vars_select(names(x), tidyselect::starts_with("op_"))] <- NA
-  x[is.na(x$pre_pat_sida_Varde), tidyselect::vars_select(names(x), tidyselect::starts_with("pre_"))] <- NA
-  x[is.na(x$post_pat_sida_Varde), tidyselect::vars_select(names(x), tidyselect::starts_with("post_"))] <- NA
-  x[is.na(x$r_pat_sida_Varde), tidyselect::vars_select(names(x), tidyselect::starts_with("r_"))] <- NA
+  if ("op_pat_sida_Varde" %in% names(x)) {
+    x[is.na(x$op_pat_sida_Varde), tidyselect::vars_select(names(x), tidyselect::starts_with("op_"))] <- NA
+  }
+  if ("pre_pat_sida_Varde" %in% names(x)) {
+    x[is.na(x$pre_pat_sida_Varde), tidyselect::vars_select(names(x), tidyselect::starts_with("pre_"))] <- NA
+  }
+  if ("post_pat_sida_Varde" %in% names(x)) {
+    x[is.na(x$post_pat_sida_Varde), tidyselect::vars_select(names(x), tidyselect::starts_with("post_"))] <- NA
+  }
+  if ("r_pat_sida_Varde" %in% names(x)) {
+    x[is.na(x$r_pat_sida_Varde), tidyselect::vars_select(names(x), tidyselect::starts_with("r_"))] <- NA
+  }
 
   return(x)
 }
